66862151a97f3e942db7619a77876e6458c40e11
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-05-29 15:38:32 +0100

    made borderRadius 3 and icons bigger

--- server/resources/[method]/method-hud/ui/src/containers/Settings/index.jsx
+++ server/resources/[method]/method-hud/ui/src/containers/Settings/index.jsx
@@ -14,6 +14,7 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@mui/styles';
 import Bars from './forms/status-types/Bars';
+import Circles from './forms/status-types/Circles';
 import Nui from '../../util/Nui';
 import { useEffect } from 'react';
 import Minimap from './forms/layouts/Minimap';
@@ -57,6 +58,10 @@ const barTypes = [
         value: 'bars',
         label: 'Bars',
     },
+    {
+        value: 'circles',
+        label: 'Circles'
+    }
 ];
 
 const useStyles = makeStyles((theme) => ({
