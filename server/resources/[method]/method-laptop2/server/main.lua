918cfc9a17cd50b24a8dc6e01fab3d129bc183ec
ImJamey <162184644+ImJamey@users.noreply.github.com>
2024-05-30 10:29:54 +0100

    :red_circle:

--- server/resources/[method]/method-laptop2/server/main.lua
+++ server/resources/[method]/method-laptop2/server/main.lua
@@ -52,7 +52,7 @@ function defaultSettings()
 		wallpaper = "wallpaper",
 		texttone = "notification.ogg",
 		colors = {
-			accent = "#1a7cc1",
+			accent = "#d50000",
 		},
 		zoom = 75,
 		volume = 100,
